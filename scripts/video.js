73b2a54387ab8edc3e27ba312de1206964131a34
MDTAHSINURRAHMAN <mdtahsinurrahman1210@gmail.com>
2024-10-03 00:04:16 +0600

    Recap the Mechanism, Load Videos and Display Cards on the UI

--- scripts/video.js
+++ scripts/video.js
@@ -9,6 +9,38 @@ const loadCategories = () => {
     .catch(error => console.log(error))
 }
 
+const loadVideos = () => {
+    // Fetch the Data
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then(res => res.json())
+    .then(data => displayVideos(data.videos))
+    .catch(error => console.log(error))
+}
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById('videos');
+    videos.forEach(video => {
+        const card = document.createElement("div");
+        card.classList = "card card-compact";
+        card.innerHTML = 
+        `
+        <figure>
+            <img
+            src= ${video.thumbnail}
+            alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `
+        videoContainer.append(card);
+    });
+}
+
 // Create Display Categories
 const displayCategories = (categories) => {
     const categoryContainer = document.getElementById('categories');
@@ -19,7 +51,8 @@ const displayCategories = (categories) => {
         button.innerText = item.category;
         // Add Button to Category Container
         categoryContainer.append(button);
-    })
+    });
 }
 
 loadCategories();
+loadVideos();
